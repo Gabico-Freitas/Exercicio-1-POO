4ce672ebb3c36248a1ed48b77e3fde44c6080d3e
Gabico-Freitas <gabriel.f.silva278@gmail.com>
2024-09-02 18:07:56 -0300

    Criação das colunas pra separar os parâmetros

--- Tabela.java
+++ Tabela.java
@@ -1,7 +1,8 @@
-//Código de exemplo para ler o txt
+//Código para ler um txt
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 class Tabela{
     public static void main(String[] args) {
@@ -9,9 +10,17 @@ class Tabela{
 
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
             String linha;
-            while ((linha = br.readLine()) != null) {
-                System.out.println(linha);
-            }
+            
+            //código para imprimir todas as linhas do txt
+              while ((linha = br.readLine()) != null) {
+                if (linha.startsWith("| Classificação")|| linha.trim().isEmpty() || linha.startsWith("|---")) {
+                    continue;
+                }
+                //Com essa separação cada coisa entre | vira um item do Array
+                String[] coluna=linha.trim().split("\\|");
+                System.out.println(coluna[2]);
+              }
+              
         } catch (IOException e) {
             e.printStackTrace();
         }
