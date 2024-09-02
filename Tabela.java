cef554cb30a02132092c5f8c75a6514b054c33c2
Gabico-Freitas <gabriel.f.silva278@gmail.com>
2024-09-02 15:57:37 +0000

    Código modelo para ler arquivo txt

--- Tabela.java
+++ Tabela.java
@@ -1,3 +1,19 @@
+//Código para ler um txt
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 class Tabela{
-    
+    public static void main(String[] args) {
+        String caminhoArquivo = "/workspaces/Exercicio-1-POO/tabela.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
