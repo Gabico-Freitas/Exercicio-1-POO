6856ed67c6d0b333da8fd25f1e8b729b4ead327f
Gabico-Freitas <gabriel.f.silva278@gmail.com>
2024-09-03 16:00:55 +0000

    Criação do método Menu() e formatação da classe App

--- App.java
+++ App.java
@@ -1,11 +1,67 @@
-
+import java.util.Scanner;
 import java.util.ArrayList;
 class App{
+    private static Scanner teclado = new Scanner(System.in);
     public static void main (String [] args){
+        
         Campeonato jogo= new Campeonato();
         Time [] lista;
+        int escolha;
         lista=jogo.lerArquivo();
-        
-        System.out.println(lista[0].getNome());
+        do {
+            menu();
+            escolha=teclado.nextInt();
+            switch (escolha) {
+                case 1:
+                    System.out.printf("O time com a melhor defesa do campeonato foi o "+ escolha);
+                    break;
+                case 2:
+                    System.out.printf("O time com o melhor ataque do campeonato foi o "+ escolha);
+                    break;
+                case 3:
+                    System.out.printf("O time com o pior ataque do campeonato foi o "+ escolha);    
+                    break;
+                case 4:
+                    System.out.printf("O time com a pior defesa do campeonato foi o "+ escolha);
+                    break;
+                case 5:
+                    System.out.printf("A média de gols por partida no campeonato foi de ");  
+                    break;
+                case 6:
+                    System.out.printf("Quer a porcentagem de vitorias em relação ao numero de jogos total do campeonato ou específica de algum time?");
+                    System.out.printf("\nDigite 1 para total e 2 para individual\n");
+                    int opcao=teclado.nextInt();                   
+                    if(opcao==1){
+                        System.out.printf("\nPorcentagem de vitorias em relação ao numero de jogos total do campeonato foi de "+ escolha);
+                    }
+                    else{
+                        System.out.printf("\nPorcentagem de vitorias em relação ao numero de jogos individual do time escolhido foi de"+ escolha);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+        }while (escolha!=7);
+        teclado.close();
     }
+    //Método para esperar antes de aparecer o menu, não está funcionando corretamente
+    // public static void waitLine() {
+    //     System.out.println("\nPressione Enter para continuar...");
+    //     teclado.nextLine();
+    // }
+    
+    //Método de menu, pode ser feito em outro local futuramente
+    public static void menu(){
+        System.out.printf("\f1 Time com a melhor defesa (menos gols contra)?");
+        System.out.printf("\n2 Time com o melhor ataque do campeonato (mais gols feitos)");
+        System.out.printf("\n3 Time com o pior ataque (menos gols feitos)?");
+        System.out.printf("\n4 Time com a pior defesa (mais gols contra)?");
+        System.out.printf("\n5 Média de gols por partida no campeonato");
+        System.out.printf("\n6 Porcentagem de vitorias em relação ao numero de jogos(total do campeonato ou individual)");
+        System.out.printf("\n7 Encerrar a aplicação");
+        System.out.printf("\nO que deseja fazer?\n");
+    }
+
+
 }
